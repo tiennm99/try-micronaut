bcfb359259e94c9f53775f1c1e6bd9ebea7695be
tiennm99 <tiennm99@outlook.com>
2024-10-14 19:00:12 +0700

    [Init]

---
+++ src/test/java/com/miti99/TryMicronautTest.java
@@ -0,0 +1,21 @@
+package com.miti99;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class TryMicronautTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
